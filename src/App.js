4aba8245bb15024c871572d0f9fc84debbecfaf3
Sunmeng <sunmengwork@foxmail.com>
2018-07-03 21:24:37 +0800

    简单实现受控制的数字

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            num: 0
+        };
+    }
+    handleAddGunAsync() {
+        setTimeout(() => {
+            this.setState({
+                num: this.state.num + 1
+            });
+        }, 1000);
+    }
     render() {
-        return <div>哈哈哈</div>;
+        return (
+            <div>
+                <h2>受控制的数字：{this.state.num}</h2>
+                <button onClick={() => this.setState({ num: this.state.num + 1 })}>数字加一</button>
+                <button onClick={() => this.setState({ num: this.state.num - 1 })}>数字减一</button>
+                <button onClick={this.handleAddGunAsync.bind(this)}>拖1s再加一</button>
+            </div>
+        );
     }
 }
 
