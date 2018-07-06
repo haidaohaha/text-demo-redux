ca2adaf52dc7b1ade051658ff28ed55fc29f325d
Sunmeng <sunmengwork@foxmail.com>
2018-07-06 13:57:18 +0800

    fix；

--- src/my.redux.js
+++ src/my.redux.js
@@ -1,3 +1,5 @@
+import { ReadStream } from 'tty';
+
 export function createStore(reducer) {
     let currentState = {};
     let currentListeners = [];
@@ -30,17 +32,21 @@ export function createStore(reducer) {
 // 所谓的“穿透” 没理解
 function bindActionCreator(creator, dispatch) {
     return (...args) => {
-        console.log('vip-args',args);
-        dispatch(creator(...args))
+        console.log('vip-args', args);
+        dispatch(creator(...args));
     };
 }
 export function bindActionCreators(creators, dispatch) {
-    console.log('vip-creators',creators);
-    console.log('vip-dispatch',dispatch);
-    let bound = [];
-    Object.keys(creators).forEach(v => {
-        let creator = creators[v];
-        bound[v] = bindActionCreator(creator, dispatch);
-    });
-    return bound;
+    console.log('vip-creators', creators);
+    console.log('vip-dispatch', dispatch);
+    // let bound = [];
+    // Object.keys(creators).forEach(v => {
+    //     let creator = creators[v];
+    //     bound[v] = bindActionCreator(creator, dispatch);
+    // });
+    // return bound;
+    return Object.keys(creators).reduce((rst, item) => {
+        rst[item] = bindActionCreator(creators[item], dispatch);
+        return rst; // 注意 rst 是一个对象，每次 key 是不同的，这样你就能理解了
+    }, {});
 }
